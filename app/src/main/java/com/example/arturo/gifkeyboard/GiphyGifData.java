ea016dd4d2efce425a3505848db29f2c029d190a
Arturo Corro <arturo.f.corro@gmail.com>
2016-10-22 23:42:36 -0400

    Added search functionality, only works for one image

---
+++ app/src/main/java/com/example/arturo/gifkeyboard/GiphyGifData.java
@@ -0,0 +1,18 @@
+package com.example.arturo.gifkeyboard;
+
+/**
+ * Created by Arturo on 10/22/16.
+ */
+
+public class GiphyGifData {
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
--- app/src/main/java/com/example/arturo/gifkeyboard/MainActivity.java
+++ app/src/main/java/com/example/arturo/gifkeyboard/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.arturo.gifkeyboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -17,29 +24,59 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.Headers;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
-    Button search;
+    Button Search;
     EditText display;
+
+    private ImageView gifView;
+    private GiphyGifData giphyGifData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        search = (Button) findViewById(R.id.Search);
         display = (EditText)findViewById(R.id.editText);
-        String gif_url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cute+funny+cat+kitten";
-        try{
-            retrieveGifs(gif_url);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+        gifView = (ImageView) findViewById(R.id.gifImage);
+        Search = (Button) findViewById(R.id.Search);
+        Search.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View v) {
+                String searchText = display.getText().toString(); //get the search Text entered by the user
+                try{
+                    retrieveGifs(searchText); //call the retrieve gif method with parameter: searchText
+                }catch(Exception e){
+                    Toast.makeText(getApplicationContext(),"ERROR WHILE RETRIEVING GIFS",Toast.LENGTH_LONG).show();
+                    System.out.println("Error while retrieving the gifs. Exception message: " + e.getMessage());
+                }
+            }
+        });
+        Toast.makeText(getApplicationContext(),"IT WORKS", Toast.LENGTH_LONG).show();
 
     }
 
-    public void retrieveGifs(String url) throws Exception{
+    private GiphyGifData getGif(String jsonData) throws JSONException {
+        JSONObject giphy = new JSONObject(jsonData);
+        JSONObject data = giphy.getJSONObject("data");
+
+        GiphyGifData Gif = new GiphyGifData();
+        Gif.setUrl(data.getString("image_url"));
+
+        return Gif;
+    }
+
+    public void retrieveGifs(String searchText) throws Exception{
         // Retrieve Gifs from Giphy
         OkHttpClient client = new OkHttpClient();
+
+        String apiKey = "dc6zaTOxFJmzC"; //Giphy's Public API Key
+        String url = "http://api.giphy.com/v1/gifs/random" +
+                "?api_key=" +
+                apiKey +
+                "&tag=" +
+                searchText;
+
         Request request = new Request.Builder()
                 .url(url)
                 .build();
@@ -50,19 +87,48 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override public void onResponse(Call call, Response response) throws IOException {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                    }
+                });
                 if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
-                Headers responseHeaders = response.headers();
-                for (int i = 0, size = responseHeaders.size(); i < size; i++) {
-                    System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
-                }
+//                Headers responseHeaders = response.headers();
+//                for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+//                    System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+//                }
 
-                System.out.println(response.body().string());
+                try{
+                    String jsonData = response.body().string();
+                    giphyGifData = getGif(jsonData);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            displayGifs();
+                        }
+                    });
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
             }
         });
     }
 
     public void displayGifs(){
 
+        Glide.with(MainActivity.this)
+                .load(giphyGifData.getUrl())
+                .thumbnail(0.1f)
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .centerCrop()
+                .into(new GlideDrawableImageViewTarget(gifView) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> animation) {
+                        super.onResourceReady(resource, animation);
+                        //check isRefreshing
+                    }
+                });
     }
 }
