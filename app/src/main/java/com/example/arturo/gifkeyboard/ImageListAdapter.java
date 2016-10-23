50b59b34d3769cdf196a27a5299ca030d79b44cf
Arturo Corro <arturo.f.corro@gmail.com>
2016-10-23 05:17:17 -0400

    List View for Images & copy url functionality

---
+++ app/src/main/java/com/example/arturo/gifkeyboard/ImageListAdapter.java
@@ -0,0 +1,61 @@
+package com.example.arturo.gifkeyboard;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+
+import static android.R.attr.animation;
+import static android.R.attr.resource;
+
+/**
+ * Created by Arturo on 10/23/16.
+ */
+
+public class ImageListAdapter extends ArrayAdapter {
+    private Context context;
+    private LayoutInflater inflater;
+
+    private String[] imageUrls;
+
+    public ImageListAdapter(Context context, String[] imageUrls) {
+        super(context, R.layout.listview_item_image, imageUrls);
+
+        this.context = context;
+        this.imageUrls = imageUrls;
+
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (null == convertView) {
+            convertView = inflater.inflate(R.layout.listview_item_image, parent, false);
+        }
+
+        Glide
+                .with(context)
+                .load(imageUrls[position])
+                .thumbnail(0.1f)
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .centerCrop()
+                .into(new GlideDrawableImageViewTarget((ImageView) convertView) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> animation) {
+                        super.onResourceReady(resource, animation);
+                        //check isRefreshing
+                    }
+                });
+
+        return convertView;
+    }
+
+}
--- app/src/main/java/com/example/arturo/gifkeyboard/MainActivity.java
+++ app/src/main/java/com/example/arturo/gifkeyboard/MainActivity.java
@@ -4,13 +4,15 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -18,8 +20,8 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
-import com.bumptech.glide.util.LruCache;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -28,17 +30,14 @@ import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import okhttp3.Headers;
 
 import java.io.IOException;
-import java.net.URI;
-import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
     Button Search;
     EditText display;
+    ListView listView;
 
-    private ImageView gifView;
     private GiphyGifData giphyGifData;
 
     @Override
@@ -47,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         display = (EditText)findViewById(R.id.editText);
-        gifView = (ImageView) findViewById(R.id.gifImage);
+        listView = (ListView)findViewById(R.id.l1);
         Search = (Button) findViewById(R.id.Search);
         Search.setOnClickListener( new View.OnClickListener() {
             public void onClick(View v) {
@@ -60,28 +59,41 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        gifView.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v)
-            {
-                String[] urlsData = giphyGifData.getUrl();
-                System.out.println("WORKING");
-                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText("The link to selected gif", urlsData[0]);
-                clipboard.setPrimaryClip(clip);
+//        gifView.setOnClickListener(new View.OnClickListener(){
+//            public void onClick(View v)
+//            {
+//                String[] urlsData = giphyGifData.getUrl();
+//                System.out.println("WORKING");
+//                ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+//                ClipData clip = ClipData.newPlainText("The link to selected gif", urlsData[0]);
+//                clipboard.setPrimaryClip(clip);
+//            }
+//        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                copyUrl(position);
             }
         });
-        Toast.makeText(getApplicationContext(),"IT WORKS", Toast.LENGTH_LONG).show();
 
     }
 
     private GiphyGifData getGif(String jsonData) throws JSONException {
+        System.out.println("HELLO");
         GiphyGifData Gif = new GiphyGifData();
-        String[] urls = new String[100];
-        JSONObject giphy = new JSONObject(jsonData.trim());
-        JSONObject data = giphy.getJSONObject("data");
-        int i;
-        for(i = 0; i < data.names().length(); i++) {
-            urls[i] = data.getString("image_url");
+
+        JSONObject giphy = new JSONObject(jsonData);
+        JSONArray data = giphy.getJSONArray("data");
+        String[] urls = new String[data.length()];
+        System.out.println(jsonData);
+        System.out.println("data length: " + data.length());
+
+        for(int i = 0; i < data.length(); i++){
+            JSONObject image = (JSONObject)data.get(i);
+            JSONObject type = image.getJSONObject("images").getJSONObject("downsized");
+            urls[i] = type.getString("url");
+            System.out.println("IMAGE URL: " + urls[i]);
         }
         Gif.setUrl(urls);
         return Gif;
@@ -92,11 +104,11 @@ public class MainActivity extends AppCompatActivity {
         OkHttpClient client = new OkHttpClient();
 
         String apiKey = "dc6zaTOxFJmzC"; //Giphy's Public API Key
-        String url = "http://api.giphy.com/v1/gifs/random" +
-                "?api_key=" +
-                apiKey +
-                "&tag=" +
-                searchText;
+        String url = "http://api.giphy.com/v1/gifs/search?" +
+                "q=" + searchText +
+                "&api_key=" +
+                apiKey;
+
 
         Request request = new Request.Builder()
                 .url(url)
@@ -116,11 +128,6 @@ public class MainActivity extends AppCompatActivity {
                 });
                 if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
-//                Headers responseHeaders = response.headers();
-//                for (int i = 0, size = responseHeaders.size(); i < size; i++) {
-//                    System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
-//                }
-
                 try{
                     String jsonData = response.body().string();
                     giphyGifData = getGif(jsonData);
@@ -139,20 +146,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void displayGifs(){
 
+        listView.setAdapter(new ImageListAdapter(MainActivity.this, giphyGifData.getUrl()));
 
-        String urls[] = giphyGifData.getUrl();
-        int i = 0;
-        Glide.with(MainActivity.this)
-                .load(urls[i])
-                .thumbnail(0.1f)
-                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-                .centerCrop()
-                .into(new GlideDrawableImageViewTarget(gifView) {
-                    @Override
-                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> animation) {
-                        super.onResourceReady(resource, animation);
-                        //check isRefreshing
-                    }
-                });
+
+    }
+
+    public void copyUrl (int position){
+        String[] array  = giphyGifData.getUrl();
+        String url = array[position];
+
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("GIF Image", url);
+        clipboard.setPrimaryClip(clip);
+
+        Toast.makeText(getApplicationContext(),"URL copied to clipboard",Toast.LENGTH_LONG).show();
+
+
     }
 }
