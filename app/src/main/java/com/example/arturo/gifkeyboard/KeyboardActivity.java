294ee997fe38dcdf52afe8698b5b176ac1c6ab54
barriossteven <barriossteven@gmail.com>
2016-10-23 06:25:40 -0400

    enable default keyboard

--- app/src/main/java/com/example/arturo/gifkeyboard/KeyboardActivity.java
+++ app/src/main/java/com/example/arturo/gifkeyboard/KeyboardActivity.java
@@ -1,16 +1,50 @@
 package com.example.arturo.gifkeyboard;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class KeyboardActivity extends AppCompatActivity {
+    Button enable;
+    Button set;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keyboard);
+     ///  ActionBar bar = getActionBar();
+        //bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
+
+        set = (Button)findViewById(R.id.set);
+        enable = (Button)findViewById(R.id.enable);
+
+        enable.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivityForResult(new Intent(android.provider.Settings.ACTION_INPUT_METHOD_SETTINGS), 0);
+
+            }
+        });
+
+        set.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
+
+            }
+        });
     }
 
     @Override
