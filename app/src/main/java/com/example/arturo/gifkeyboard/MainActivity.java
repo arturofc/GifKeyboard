15823dd148e65f2238971ba4b74dbb3aa60d8a4b
Arturo Corro <arturo.f.corro@gmail.com>
2016-10-22 20:31:27 -0400

    retrieve gifs moved inside Main Activity

--- app/src/main/java/com/example/arturo/gifkeyboard/MainActivity.java
+++ app/src/main/java/com/example/arturo/gifkeyboard/MainActivity.java
@@ -9,9 +9,18 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.Headers;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText display;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +29,13 @@ public class MainActivity extends AppCompatActivity {
 
         display = (EditText)findViewById(R.id.testDisplay);
         String gif_url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cute+funny+cat+kitten";
-        //new RetrieveGifs().execute(gif_url);
+        try{
+            retrieveGifs(gif_url);
+        }catch(Exception e){
+
+        }
+
+
 
     }
 
@@ -35,7 +50,28 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void retrieveGifs(String url) throws Exception{
+        // Retrieve Gifs from Giphy
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
 
+        client.newCall(request).enqueue(new Callback() {
+            @Override public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
 
+            @Override public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
+                Headers responseHeaders = response.headers();
+                for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                    System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                }
+
+                System.out.println(response.body().string());
+            }
+        });
+    }
 }
